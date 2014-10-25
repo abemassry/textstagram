3f92a046ea3310b0a6643ac1645a0b60cb0b37fc
abemassry <abe@massindustries.org>
2014-10-24 21:35:02 -0400

    pact.js

---
+++ pact.js
@@ -0,0 +1,27 @@
+var appio = require('./app.js').io;
+
+var getId = function() {
+  var randInt = getRandomInt(1, 999999999999999);
+  var diffRandInt = getRandomInt(1, 999999999999999);
+  var randStringLength = getRandomInt(1, 40);
+  randInt = randInt.toString();
+  diffRandInt = diffRandInt.toString();
+  var randString = 'v84by0cmomcmw0-amckajapsodc9qj].dcq3785$';
+  randString = randString.substring(1, randStringLength);
+  randInt = randInt + randString + diffRandInt;
+  var sha512 = crypto.createHash('sha512');
+  sha512.update(randInt);
+  var resultHash = sha512.digest("hex");
+  var result = resultHash;
+  var length = 32;
+  var trimmedResult = result.substring(0, length);
+  return trimmedResult;
+};
+
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+exports.getId = getId;
+exports.io = appio;
+
