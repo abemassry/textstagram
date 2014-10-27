7daaaa4548ab905f0e4761e70b3b08d3683a251a
abemassry <abe@massindustries.org>
2014-10-26 21:55:03 -0400

    editing navbar

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var pact = require('../pact.js');
 
 exports.main = function(req, res){
-  res.render('index', { title: 'Welcome' });
+  res.render('index', { title: 'Welcome',
+                        user: false
+                      });
 };
