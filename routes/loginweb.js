beac782649ec96efe43a7b9d7421579dc0a390f9
abemassry <abe@massindustries.org>
2014-11-13 21:39:33 -0500

    adding login route

---
+++ routes/loginweb.js
@@ -0,0 +1,39 @@
+var pact = require('../pact.js');
+
+exports.main = function(req, res){
+  if (req.user) {
+    res.redirect('/');
+  } else {
+    var userName = req.param('username');
+    var password = req.param('password');
+    var now = new Date();
+    pact.models.users.findOne({user_name: userName}, function(err, doc) {
+      if(err) return handleError(err);
+      if (doc) {
+        var hash = doc.password;
+        if (bcrypt.compareSync(passwd, hash)) {
+          doc.last_active = now;
+          doc.save(function(err) {
+            if(err) { throw err; }
+          });
+          req.session.logged = true;
+          req.session.user = userName;
+          res.redirect('/');
+        } else {
+          res.render('login', {
+            title: 'Login',
+            user: false,
+            errMessage: 'Username or Password Incorrect'
+          });
+        }
+      } else {
+        res.render('login', {
+          title: 'Login',
+          user: false,
+          errMessage: 'Username or Password Incorrect'
+        });
+      }
+    });
+  }
+
+};
