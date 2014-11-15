8fb3285ce35b2f01c56c5cd2de035ffa5c4ff6e5
abemassry <abe@massindustries.org>
2014-11-14 21:50:22 -0500

    have to do a profile page

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,7 @@
 var pact = require('../pact.js');
 
 exports.main = function(req, res){
-  res.render('index', { title: 'Welcome',
+  res.render('profile', { title: 'Welcome',
                         user: req.session.user
                       });
 };
