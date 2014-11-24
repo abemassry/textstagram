bf59fe30379705544a98c2b66284d3f7a011c9c3
abemassry <abe@massindustries.org>
2014-11-24 10:50:05 -0500

    adding user route and view

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+var pact = require('../pact.js');
+
+exports.main = function(req, res){
+  if (req.user) {
+    var username = req.sesssion.user;
+  }
+  pact.models.findOne({name: req.params.name}, function(err, data){
+    if (data) {
+      pact.models.posts.find({submitted_by: req.params.name }).limit(30).sort('-date').exec(function(err, posts){
+        res.render('user', {  title: req.params.name,
+                              user: username,
+                              posts: posts
+                            }
+                  });
+    } else {
+      res.redirect('/404');
+    }
+  });
+};
