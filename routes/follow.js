7aec74ef5dcae085804abe0f8d2b08992edafee7
abemassry <abe@massindustries.org>
2015-01-10 21:39:16 -0500

    follow button, some problem with sessions

--- routes/follow.js
+++ routes/follow.js
@@ -1,22 +1,30 @@
 var pact = require('../pact.js');
 
 exports.main = function(req, res){
+  console.log('made it inside function');
   if (req.session.user) {
+    console.log('made it inside function and if statement');
     var userName = req.session.user;
     var now = new Date();
-    pact.models.users.findOne({user_name: req.param('tofollow')}, function(err, doc) {
+    pact.models.users.findOne({user_name: userName}, function(err, userModel) {
       if(err) return handleError(err);
       if (doc) {
         var follow_name = req.param('tofollow');
-        pact.models.users.findOne(post_id, function(err, post){
+        pact.models.users.findOne({user_name: follow_name}, function(err, followModel){
           if (err) return handleError(err);
-          post.comments.push({user: userName, body: req.param('comment'), date_commented: now});
-          var doc = post.comments[0];
-          console.log(doc);
-          post.save(function(err) {
+          userModel.following.push({user_name: follow_name, timestamp: now});
+          userModel.save(function(err) {
             if(err) { throw err; }
-            console.log('saved');
-            res.redirect('/p/'+post_id);
+            console.log('userModel saved');
+          });
+          followModel.followers.push({user_name: userName, timestamp: now});
+          followModel.save(function(err) {
+            if(err) { throw err; }
+            console.log('followModel saved');
+            followModel.followers.count(function(err, c) {
+              console.log('the count is c');
+              res.send(c);
+            });
           });
         });
       }
