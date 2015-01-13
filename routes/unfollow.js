033ecc4fddbabf6323fbc3a2d908c297a652f6db
abemassry <abe@massindustries.org>
2015-01-12 21:53:27 -0500

    working on unfollow

--- routes/unfollow.js
+++ routes/unfollow.js
@@ -9,22 +9,38 @@ exports.main = function(req, res){
     pact.models.users.findOne({user_name: userName}, function(err, userModel) {
       if(err) return handleError(err);
       if (userModel) {
+        userModel.following.forEach(function(follower) {
+          var followerId = follower.id;
+          if (follower.user_name == userName) {
+            userModel.following.id(followerId).remove();
+            userModel.save(function(err) {
+              if(err) {throw err;}
+              console.log('following removed');
+            });
+          }
+        });
         var unfollow_name = req.param('tounfollow');
         pact.models.users.findOne({user_name: unfollow_name}, function(err, unfollowModel){
           if (err) return handleError(err);
-          userModel.following.remove({user_name: unfollow_name});
-          userModel.save(function(err) {
-            if(err) { throw err; }
-            console.log('following removed');
-          });
-          unfollowModel.followers.remove({user_name: userName});
-          unfollowModel.save(function(err) {
-            if(err) { throw err; }
-            console.log('follower removed');
-            var count = 0;
-            console.log('the count is :');
-            console.log(unfollowModel.followers.length);
-            res.end(unfollowModel.followers.length.toString());
+          unfollowModel.followers.forEach(function(follower) {
+            var unfollowerId = unfollowModel.id;
+            if (follower.user_name == unfollow_name) {
+              console.log('unfollowerId');
+              console.log(unfollowerId);
+              console.log('follower.user_name');
+              console.log(follower.user_name);
+              
+
+              unfollowModel.followers.id(unfollowerId).remove();
+              unfollowModel.save(function(err) {
+                if(err) { throw err; }
+                console.log('follower removed');
+                var count = 0;
+                console.log('the count is :');
+                console.log(unfollowModel.followers.length);
+                res.end(unfollowModel.followers.length.toString());
+              });
+            }
           });
         });
       }
