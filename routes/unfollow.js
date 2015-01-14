651d92b111ab0c9965f140a545e48236d0c81659
abemassry <abe@massindustries.org>
2015-01-13 22:35:36 -0500

    working on unfollow no matching document found

--- routes/unfollow.js
+++ routes/unfollow.js
@@ -1,17 +1,21 @@
 var pact = require('../pact.js');
 
 exports.main = function(req, res){
-  console.log('made it inside function');
+  console.log('made it inside unfollow function');
   if (req.session.user) {
-    console.log('made it inside function and if statement');
+    var unfollow_name = req.param('tounfollow');
+    console.log('made it inside unfollow function and if statement');
     var userName = req.session.user;
     var now = new Date();
     pact.models.users.findOne({user_name: userName}, function(err, userModel) {
+      console.log('found one');
       if(err) return handleError(err);
       if (userModel) {
+        console.log('got a user model');
         userModel.following.forEach(function(follower) {
-          var followerId = follower.id;
-          if (follower.user_name == userName) {
+          if (follower.user_name == unfollow_name) {
+            console.log('got a match');
+            var followerId = follower.id;
             userModel.following.id(followerId).remove();
             userModel.save(function(err) {
               if(err) {throw err;}
@@ -19,16 +23,15 @@ exports.main = function(req, res){
             });
           }
         });
-        var unfollow_name = req.param('tounfollow');
         pact.models.users.findOne({user_name: unfollow_name}, function(err, unfollowModel){
           if (err) return handleError(err);
-          unfollowModel.followers.forEach(function(follower) {
-            var unfollowerId = unfollowModel.id;
-            if (follower.user_name == unfollow_name) {
+          unfollowModel.followers.forEach(function(unfollower) {
+            if (unfollower.user_name == unfollow_name) {
+              var unfollowerId = unfollower.id;
               console.log('unfollowerId');
               console.log(unfollowerId);
               console.log('follower.user_name');
-              console.log(follower.user_name);
+              console.log(unfollower.user_name);
               
 
               unfollowModel.followers.id(unfollowerId).remove();
