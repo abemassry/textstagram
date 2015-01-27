6269b120255668dd55f0b8c06589e008d154cf6c
abemassry <abe@massindustries.org>
2015-01-26 20:58:23 -0500

    profile page starting on fix

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,22 @@
 var pact = require('../pact.js');
 
 exports.main = function(req, res){
-
-  res.render('profile', { title: 'Welcome',
-                        user: req.session.user
-                      });
+  if (req.session.user) {
+    pact.models.users.findOne({user_name: req.session.user}, function(err, userModel){
+      if(userModel) {
+        pact.models.posts.find({submitted_by: req.session.user }).limit(30).sort('-date').exec(function(err, posts){
+          res.render('profile', { title: 'Profile',
+                                user: req.session.user,
+                                followers: userModel.followers.length,
+                                following: userModel.following.length,
+                                posts: posts
+                              });
+        });
+      } else {
+        res.redirect('/login');
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
 };
