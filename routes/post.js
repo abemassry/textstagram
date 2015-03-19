41fbd37c5168b2ae649bf11a8ad2c400b5aa43f9
Abe Massry <amassry@linode.com>
2015-03-18 22:21:11 -0400

    button push works, need to work on js handling

--- routes/post.js
+++ routes/post.js
@@ -6,7 +6,6 @@ exports.main = function(req, res){
   }
   pact.models.posts.findById(req.params.id, function(err, data){
     if(data) {
-      
       res.render('post', {  title: 'Post by '+data.submitted_by,
                   user: username,
                   post: data
