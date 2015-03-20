35502429da620fc0b97af26a2d838eb53bf52b1e
Abe Massry <amassry@linode.com>
2015-03-19 22:06:57 -0400

    fixed user display problem

--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,10 @@
 var pact = require('../pact.js');
 
 exports.main = function(req, res){
-  if (req.user) {
-    var username = req.sesssion.user;
-  }
   pact.models.posts.findById(req.params.id, function(err, data){
     if(data) {
       res.render('post', {  title: 'Post by '+data.submitted_by,
-                  user: username,
+                  user: req.session.user,
                   post: data
                 }
       );
